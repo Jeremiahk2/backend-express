45ab9e747af4716ab4dcdcce210829f993240707
Jeremiah Knizley <TheMainMoe@gmail.com>
2025-05-06 00:41:10 -0400

    Updated database SSL settings.

--- src/db.ts
+++ src/db.ts
@@ -7,4 +7,5 @@ export const dbClient = new Client({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    ssl: {rejectUnauthorized: false}
 });
